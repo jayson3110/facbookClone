42cf36d60e586dfc272954b44b0b18a12fdb389e
jaysonrighthere <jayson7498@gmail.com>
2021-06-23 10:15:25 +0700

    done auth , navbar

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Image from "next/image";
-import {BellIcon,ChatIcon,ChevranDownIcon,HomeIcon,UserGroupIcon,ViewGridIcon}  from '@heroicons/react/solid'
+import {BellIcon,ChatIcon,ChevronDownIcon,HomeIcon,UserGroupIcon,ViewGridIcon}  from '@heroicons/react/solid'
 import {FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon} from  '@heroicons/react/outline'
 import HeaderIcon from "./HeaderIcon";
+import {signOut ,useSession} from "next-auth/client";
+
 
 export default function Header() {
+
+	const [session] = useSession()
 	return (
 		<div className="sticky top-0 z-58 bg-white flex items-center p-2 lg:px-5 shadow-md">
 		   {/* Left  */}
@@ -17,7 +21,7 @@ export default function Header() {
 		        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
 
 		            <SearchIcon className="h-6 text-gray-600"  />
-		            <input  className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
+		            <input  className="hidden md:inline-flex 1flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
 
 
 		        </div>
@@ -42,6 +46,31 @@ export default function Header() {
 
 
 		   {/* Right  */}
+
+		   <div className="flex items-center sm:space-x-2 justify-end">
+
+		      {/* Profile pic */}
+		      <Image 
+		      onClick = {signOut}
+		      className="rounded-full cursor-pointer"
+		      src={session.user.image}
+		      width="40"
+		      height="40"
+		      layout="fixed"
+
+
+		      />
+		      <p className="whitespace-nowrap font-semibold pr-3">{session.user.name}</p>
+		      <ViewGridIcon  className="icon"/>
+		      <ChatIcon  className="icon"/>
+		      <BellIcon className="icon"/>
+		      <ChevronDownIcon className="icon"/>
+
+		   </div>
+
+
+
+
 			
 		</div>
 	)
---
+++ components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useSession} from 'next-auth';
+
+export default function Sidebar() {
+	const [session, loading] = useSession()
+	return (
+		<div className="p2 mt-5 max-w-600 xl: min-w-300">
+
+		    
+			
+
+
+
+
+		</div>
+	)
+}
